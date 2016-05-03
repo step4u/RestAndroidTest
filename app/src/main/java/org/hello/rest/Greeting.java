cf75b14cc33e37fda8f99ece61ee2e11ebd193aa
JC Lee RD01 <step4u@gmail.com>
2016-05-04 01:32:02 +0900

    the first commit _ rest test

---
+++ app/src/main/java/org/hello/rest/Greeting.java
@@ -0,0 +1,17 @@
+package org.hello.rest;
+
+/**
+ * Created by sequel2 on 2016-05-04.
+ */
+public class Greeting {
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
